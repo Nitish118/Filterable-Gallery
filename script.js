206a4a1057a316e29efce611ce4eee833dd401ba
Nitish <nitishsrinivasa@gmail.com>
2024-08-04 01:22:17 +0530

    Filterable Gallery files

---
+++ script.js
@@ -0,0 +1,27 @@
+const buttons = document.querySelectorAll('.btn')
+const storeImages = document.querySelectorAll('.store-item')
+
+function displayImages() {
+    buttons.forEach((button) => {
+        button.addEventListener('click', (e) => {
+            e.preventDefault()
+    
+            const filter = e.target.dataset.filter
+    
+            storeImages.forEach((item) => {
+                if (filter === 'all') {
+                    item.style.display = 'block'
+                } else {
+                    if (item.classList.contains(filter)) {
+                        item.style.display = 'block'
+                    } else {
+                        item.style.display = 'none'
+                    }
+                }
+            })
+    
+        })
+    })
+}
+
+displayImages();
